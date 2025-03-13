8618bffe071afdbda71ef69517fa84b61bc109b1
dalmat-shabani <dalmat.shabani90@gmail.com>
2025-03-13 19:13:47 +0100

    a

--- header.php
+++ header.php
@@ -19,6 +19,13 @@
     </section>    
     <section class="menu-area">
         <nav class="main-menu">
+            <button class="check-button">
+                <div class="menu-icon">
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                    <div class="bar3"></div>
+                </div>
+            </button>
            <?php wp_nav_menu(
             array('theme-location' => 'wp_devs_main_menu', 'depth' => 2)
            ); ?>
