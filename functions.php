b3b77b27b646aa4ecdffedf157de89685a4b9980
dalmat-shabani <dalmat.shabani90@gmail.com>
2025-03-25 19:14:04 +0100

    a

--- functions.php
+++ functions.php
@@ -15,4 +15,80 @@ register_nav_menus(
     )
     );
 
+
+function dstheme_config(){
+    register_nav_menus(
+        array(
+            'wp_devs_main_menu' => 'Main Menu',
+            'wp_devs_footer_menu' => 'Footer Menu'
+        )
+    );
+
+    $args = array(
+        'height' => 225,
+        'width' => 1920
+    );
+
+    add_theme_support('custom-header',$args);
+    add_theme_support('post-thumbnails');
+    add_theme_support('custom-logo', array(
+        'width'> 200,
+        'height' => 110,
+        'flex-height' => true,
+        'flex-width' => true
+    ));
+
+    }
+
+add_action('after_setup_theme', 'dstheme_config', 0);
+
+add_action('widgets_init', 'dstheme_sidebars');
+function dstheme_sidebars(){
+    register_sidebar(
+        array(
+            'name' => 'Blog Sidebar',
+            'id' => 'sidebar-blog',
+            'description' => 'This is the Blog Sidebar. You can add your widgets here.',
+            'before_widget' => '<div class = "widget-title">',
+            'after_widget' => '</div>',
+            'before_title' => '<h4 class = "widget-title">',
+            'after_title' => '</h4>'
+        )
+    );
+    register_sidebar(
+        array(
+            'name' => 'Service 1',
+            'id' => 'services-1',
+            'description' => 'First service area',
+            'before_widget' => '<div class = "widget-wrapper">',
+            'after_widget' => '</div>',
+            'before_title' => '<h4 class = "widget-title">',
+            'after_title' => '</h4>'
+        )
+    );
+    register_sidebar(
+        array(
+            'name' => 'Services-2',
+            'id' => 'services-2',
+            'description' => 'Second service area',
+            'before_widget' => '<div class = "widget-wrapper">',
+            'after_widget' => '</div>',
+            'before_title' => '<h4 class = "widget-title">',
+            'after_title' => '</h4>'
+        )
+    );
+    register_sidebar(
+        array(
+            'name' => 'Service-3',
+            'id' => 'service-3',
+            'description' => 'Third service area',
+            'before_widget' => '<div class = "widget-wrapper">',
+            'after_widget' => '</div>',
+            'before_title' => '<h4 class = "widget-title">',
+            'after_title' => '</h4>'
+        )
+    );
+} 
+
+
 ?>
--- header.php
+++ header.php
@@ -11,7 +11,15 @@
 <header>
     <section class="top-bar">
         <div class="logo">
-            Logo
+            <?php 
+            if(has_custom_logo() ){
+                the_custom_logo();
+            }else{
+                ?>
+                 <a href="<?php echo home_url( '/') ?>" <span><?php bloginfo (' name ') ?>  </span></a>
+                <?php
+            }
+             ?>
         </div>
         <div class="searchbox">
             Search Box
