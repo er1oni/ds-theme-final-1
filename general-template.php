14c67040af10742b13f442e3230e213d3f647489
er1oni <erjongashi_@hotmail.com>
2025-03-27 19:14:08 +0100

    ...

--- general-template.php
+++ general-template.php
@@ -1,26 +1,25 @@
-<?php get_header(); ?>
+<?php
+/*
+Template Name: General Template
+*/
+?>
 <?php get_header(); ?>
 <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height ?>" 
 width="<?php echo get_custom_header()->width ?>" alt="">
-        <div id="content" class="site-content">
+        
+
+<div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
-                    
                     <section class="home-blog">
                         <div class="container">
-                            <div class="blog-items">
+                            <div class="general-template">
                                 <?php
                                 if( have_posts() ):
                                     while( have_posts() ): the_post();
                                         ?>
                                             <article>
-                                                <h2><?php the_title(); ?></h2>
-                                                <?php the_post_thumbnail(array(275,275)); ?>
-                                                <div class="meta-info">
-                                                    <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link();?></p>
-                                                    <p>Catgeries: <?php the_category(); ?> </p>
-                                                    <p> <?php the_tags(); ?></p>
-                                                </div>
+                                                <h1><?php the_title(); ?></h1>
                                                 <?php the_content(); ?>
                                             </article>
                                         <?php
@@ -29,7 +28,6 @@ width="<?php echo get_custom_header()->width ?>" alt="">
                                     <p>Nothing to be displayed!</p>          
                                 <?php endif; ?>
                             </div>
-                            <?php get_sidebar(); ?>
                         </div>
                     </section>
                 </main>
--- page.php
+++ page.php
@@ -5,31 +5,20 @@ width="<?php echo get_custom_header()->width ?>" alt="">
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
-                    
                     <section class="home-blog">
                         <div class="container">
-                            <div class="blog-items">
+                            <div class="page-item">
                                 <?php
-                                if( have_posts() ):
                                     while( have_posts() ): the_post();
                                         ?>
                                             <article>
-                                                <h2><?php the_title(); ?></h2>
-                                                <?php the_post_thumbnail(array(275,275)); ?>
-                                                <div class="meta-info">
-                                                    <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link();?></p>
-                                                    <p>Catgeries: <?php the_category(); ?> </p>
-                                                    <p> <?php the_tags(); ?></p>
-                                                </div>
+                                                <h1><?php the_title(); ?></h1>
                                                 <?php the_content(); ?>
                                             </article>
                                         <?php
                                     endwhile;
-                                else: ?>
-                                    <p>Nothing to be displayed!</p>          
-                                <?php endif; ?>
+                                 ?>
                             </div>
-                            <?php get_sidebar(); ?>
                         </div>
                     </section>
                 </main>
